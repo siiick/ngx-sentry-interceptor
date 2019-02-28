c1fd246e78155e6541c925ebc06b6f21e7b3417f
Nicolas Chaintron <nicolas.c@vestiairecollective.com>
2019-02-28 09:36:15 +0100

    cleanup scaffolded service and component

--- projects/ngx-sentry-interceptor/src/public_api.ts
+++ projects/ngx-sentry-interceptor/src/public_api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of ngx-sentry-interceptor
  */
 
-export * from './lib/ngx-sentry-interceptor.service';
-export * from './lib/ngx-sentry-interceptor.component';
 export * from './lib/ngx-sentry-interceptor.module';
